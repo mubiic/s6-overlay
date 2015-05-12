2721e49a39d260f158a457073d8c2f136cec7bfe
HowardMei <howardleomei@gmail.com>
2015-05-12 21:27:55 +0800

    develop: update s6-suite github download links and readme descriptions

--- getupstream.sh
+++
@@ -1,4 +0,0 @@
-git remote add upstream https://github.com/just-containers/s6-overlay 1>&2 2>/dev/null
-git remote set-url --push upstream fetchonly 1>&2 2>/dev/null
-git checkout upstream
-git pull --force upstream master
